e285868cce244bdf793ac8d2ee4743ee0439c3be
rabeya47 <rabu016833@gmail.com>
2022-02-20 13:51:45 +0600

    20/02/2022

--- lib/networks/http_helper.dart
+++ lib/networks/http_helper.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 import 'package:flutter_food_dot_com/Model/user.dart';
 import 'package:http/http.dart' as http;
 
-//var host = 'http://192.168.1.22:8081';
-var host = 'http://localhost:8081';
+var host = 'http://192.168.1.22:8081';
+//var host = 'http://localhost:8081';
 var signupApi = host+'/user/save';
 var signinApi = host+'/user/login';
 
@@ -17,16 +17,8 @@ Future<http.Response> signup(User user) async {
 
   final response = await http
       .post(Uri.parse(signupApi),headers: requestHeaders,body: jsonEncode(user.toMap()));
+       return  response;
 
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return  response;
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception(response);
-  }
 }
 
 
@@ -36,16 +28,17 @@ Future<http.Response> signin(User user) async {
 
   final response = await http
       .post(Uri.parse(signinApi),headers: requestHeaders,body: jsonEncode(user.toMap()));
+       return  response;
 
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return  response;
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
+  // if (response.statusCode == 200) {
+  //   // If the server did return a 200 OK response,
+  //   // then parse the JSON.
+  //   return  response;
+  // } else {
+  //   // If the server did not return a 200 OK response,
+  //   // then throw an exception.
+  //   throw Exception('Failed to load album');
+  // }
 }
 
 
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -10,64 +10,100 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading:IconButton(
-          icon:SvgPicture.asset("assets/icons/menu.svg",color: Colors.black45),
-          onPressed: (){},
+        leading: IconButton(
+          icon:
+              SvgPicture.asset("assets/icons/menu.svg", color: Colors.black45),
+          onPressed: () {},
         ),
         title: RichText(
           text: TextSpan(
-            style: Theme.of(context).textTheme.subtitle1?.copyWith(fontWeight: FontWeight.bold),
+            style: Theme.of(context)
+                .textTheme
+                .subtitle1
+                ?.copyWith(fontWeight: FontWeight.bold),
             children: [
-              TextSpan(text: "Food " ,style: TextStyle(color: Colors.black)),
-              TextSpan(text: "Dot " ,style: TextStyle(color: Colors.cyan)),
-              TextSpan(text: "Com " ,style: TextStyle(color: Colors.black)),
+              TextSpan(text: "Food ", style: TextStyle(color: Colors.black)),
+              TextSpan(text: "Dot ", style: TextStyle(color: Colors.cyan)),
+              TextSpan(text: "Com ", style: TextStyle(color: Colors.black)),
             ],
           ),
         ),
         actions: <Widget>[
           IconButton(
-            icon: SvgPicture.asset("assets/icons/bell.svg",color: Colors.black45),
-              onPressed: (){},
-              )
+            icon: SvgPicture.asset("assets/icons/bell.svg",
+                color: Colors.black45),
+            onPressed: () {},
+          )
         ],
-
-
       ),
+      body: Column(
+        children: [
 
-      body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.all(20),
-          padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            border: Border.all(
-              color: Colors.black12,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (Value) {},
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(50))),
+                  hintText: "search here",
+                  hintStyle: TextStyle(color: Colors.black26)),
             ),
           ),
-          child: TextField(
-            onChanged: (Value){},
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              icon: SvgPicture.asset("assets/icons/search.svg",color: Colors.black45),
-              hintText: "search here",
-              hintStyle: TextStyle(color: Colors.black26)
-            ),
-          ),
-        ),
 
+          // Expanded(
+          //   child: GridView.count(
+          //     // Create a grid with 2 columns. If you change the scrollDirection to
+          //     // horizontal, this produces 2 rows.
+          //     crossAxisCount: 2,
+          //     // Generate 100 widgets that display their index in the List.
+          //     children: List.generate(4, (index) {
+          //       return Card(
+          //         shape: RoundedRectangleBorder(
+          //           borderRadius: BorderRadius.circular(15.0),
+          //         ),
+          //         elevation: 4,
+          //         child: Column(
+          //           children: [
+          //             SizedBox(
+          //               height: 10,
+          //             ),
+          //
+          //             Text(
+          //               'Item $index',
+          //               style: Theme.of(context).textTheme.headline5,
+          //             ),
+          //
+          //           ],
+          //         ),
+          //       );
+          //     }),
+          //   ),
+          // ),
+
+         Container(
+           margin: EdgeInsets.only(bottom: 15),
+           padding: EdgeInsets.all(25),
+           decoration: BoxDecoration(
+             color: Colors.white,
+             shape: BoxShape.circle,
+             
+           ),
+           child: Image(
+             image:  AssetImage('assets/images/burger.jpg'), fit: BoxFit.cover),
+           )
+
+
+
+        ],
       ),
-
     );
   }
 }
